d11491f3cd1f32a9137d70c716c0887b89c25e60
HyunJi-2 <aaa9013@gmail.com>
2019-05-29 13:00:57 +0900

    loading commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import PopupUploade from "./component/PopupUploade";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            percentage: 0
+        };
+    }
+    PopupOpen = () => {
+        this.setState({open: true});
+
+        const uploading = setInterval(() => {
+            this.setState({percentage : this.state.percentage + 1});
+            // console.log(this.state.percentage);
+            if(this.state.percentage === 100){
+                clearInterval(uploading);
+            }
+        }, 10);
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <button className="btn_type7" onClick={this.PopupOpen}>업로드</button>
+                <PopupUploade percentage={this.state.percentage} open={this.state.open}/>
+            </div>
+        );
+    }
 }
 
 export default App;
